65de30179fed2e416490c4e239c0015d78fa2291
Alisson Kendi Kohatsu <kohatsukendi@gmail.com>
2021-12-16 11:04:54 -0300

    fix: test style
    Testing style of calendar

--- src/components/Calendar/styles.js
+++ src/components/Calendar/styles.js
@@ -2,11 +2,12 @@ import styled from 'styled-components/native';
 
 export const Container = styled.View`
   width: 100%;
+  height: 100%;
 
   display: flex;
   align-items: center;
   justify-content: center;
   padding: 8px;
 
-  background: #505050;
+  background: #101010;
 `;
