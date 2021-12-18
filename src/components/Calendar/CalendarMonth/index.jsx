d6836bf27da75000135e2da5dfcef22fc2675837
Alisson Kendi Kohatsu <kohatsukendi@gmail.com>
2021-12-18 20:31:37 -0300

    fix: fix calendar
    Fixing calendar

--- src/components/Calendar/CalendarMonth/index.jsx
+++ src/components/Calendar/CalendarMonth/index.jsx
@@ -1,39 +1,80 @@
 import React, { useEffect, useState } from 'react';
-import { format } from 'date-fns';
+import {
+  startOfWeek,
+  endOfWeek,
+  startOfMonth,
+  endOfMonth,
+  eachDayOfInterval,
+  isToday,
+  format,
+  eachWeekOfInterval,
+  lastDayOfMonth,
+  startOfDay,
+  endOfDay,
+  addDays
+} from 'date-fns';
+import { ptBR } from 'date-fns/locale';
 import { v4 as uuidV4 } from 'uuid';
 
 import {
   Container,
-  WeekContainer,
   MonthLabelContainer,
   MonthLabel
 } from './styles';
-import { generateMonth } from '../../../utils/generateMonth';
 
 export function CalendarMonth() {
 
   const [daysOfMonth, setDaysOfMonth] = useState([]);
 
   const onGetDaysOnMonth = () => {
-    const genMonth = generateMonth();
-    const month = genMonth();
+    const month = [];
 
-    console.log(month)
+    const firstDayOfMonth = startOfMonth(
+      startOfDay(new Date())
+    );
+    const lastDayOfMonth = endOfMonth(
+      endOfDay(new Date())
+    );
 
-    // setDaysOfMonth(
-    //   month
-    //     .map((week) => week)
-    //     .map((day) => {
-    //       return {
-    //         key: uuidV4(),
-    //         date: day,
-    //         dateShort: format(day, "d")
-    //       }
-    //     })
-    // );
+    const firstDayOfWeek = startOfWeek(firstDayOfMonth, {
+      locale: ptBR,
+      weekStartsOn: 0
+    });
+    const lastDayOfWeek = endOfWeek(endOfDay(firstDayOfWeek), {
+      locale: ptBR,
+      weekStartsOn: 0
+    });
+
+    let daysOfWeek = eachWeekOfInterval({
+      start: firstDayOfWeek,
+      end: lastDayOfWeek
+    });
+
+    month[0] = daysOfWeek;
+    let lastDay = lastDayOfWeek;
+
+    while(month[month.length - 1][6] < lastDayOfMonth) {
+      lastDay = addDays(lastDay, 1);
+
+      const nextLastDayOfWeek = endOfWeek(endOfDay(lastDay), {
+        locale: ptBR,
+        weekStartsOn: 0
+      });
+
+      daysOfWeek = eachWeekOfInterval({
+        start: addDays(lastDayOfWeek, 1),
+        end: nextLastDayOfWeek
+      });
+
+      lastDay = nextLastDayOfWeek;
+
+      month[month.length + 1] = daysOfWeek;
+    }
+
+    console.log(month);
+
+    // setDaysOfMonth(month);
   }
-
-
 
   useEffect(() => {
     onGetDaysOnMonth();
@@ -41,15 +82,18 @@ export function CalendarMonth() {
 
   return (
     <Container>
-      {/* {daysOfMonth.map((week, index) => (
-        <WeekContainer>
-          {week.map(({ dateShort, key }, indexOfDay) => (
-            <MonthLabelContainer backgroundColor="#efefef" key={key}>
-              <MonthLabel color="#404040">{dateShort}</MonthLabel>
-            </MonthLabelContainer>
-          ))}
-        </WeekContainer>
-      ))} */}
+      {daysOfMonth.map(({ key, day, isToday }) => (
+        <MonthLabelContainer
+          key={key}
+          backgroundColor={isToday ? "#0e87f8" : "transparent"}
+        >
+          <MonthLabel
+            color={isToday ? "#ffffff" : "#464646"}
+          >
+            {day}
+          </MonthLabel>
+        </MonthLabelContainer>
+      ))}
     </Container>
   );
 
